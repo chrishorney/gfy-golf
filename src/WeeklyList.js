156b0dfdfb1d0c6b9cbefbbfe5ddcb159fbd9a6b
chrishorney <chris.horney@gmail.com>
2024-11-27 13:07:26 -0600

    Updating formatting

--- src/WeeklyList.js
+++ src/WeeklyList.js
@@ -21,7 +21,7 @@ function WeeklyList() {
   const [isScrolling, setIsScrolling] = useState(false);
   const scrollStartPosition = useRef(null);
 
-  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwAaikEUc_XaUJOF-Q0deSYOsX__gvTEtBaNVw8ctuZzAQSa5paJ8dPIjY9BL-y_m6I/exec';
+  const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzYh5VTVOouNTaFchj5GrBSt7YWQ6YrkKWhiXaNqGVtkbX9dhG8TTxhs9OHDLrJpbED/exec';
 
   // Create array of team numbers 1-10
   const teamNumbers = Array.from({ length: 10 }, (_, i) => i + 1);
